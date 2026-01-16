632ea04709c7f7bb5a4c75e9b3c1e3e6b1f7a6d4
SatyamDevGenie <satyamsawant54@gmail.com>
2026-01-17 04:41:47 +0530

    data sets modified -- doctors.js

--- backend/data/doctors.js
+++ backend/data/doctors.js
@@ -2,18 +2,28 @@ import bcrypt from "bcryptjs";
 
 
 const doctors = [
-    {
-      name: "Dr. Ramesh Kumar",
-      email: "ramesh.kumar@gmail.com",
-      password: bcrypt.hashSync("doctor123", 10),
-      specialization: "Veterinary Surgeon",
-      contactNumber: "9876543210",
-      profileImage: "/images/male-doctor.png", // Replace with actual image URL or path
-      notes: "Specializes in surgical procedures and emergency cases. Compassionate and thorough.",
-      isDoctor: true,
-      availability: "Mon-Tue Evening 6:00-8:00 PM"
-    }
-  ];
-  
-  export default doctors;
-  
+  {
+    name: "Dr. Ramesh Kumar",
+    email: "ramesh.kumar@gmail.com",
+    password: bcrypt.hashSync("ramesh123", 10),
+    specialization: "Veterinary Surgeon",
+    contactNumber: "9326903988",
+    profileImage: "/images/male-doctor.png", // Replace with actual image URL or path
+    notes: "Specializes in surgical procedures and emergency cases. Compassionate and thorough.",
+    isDoctor: true,
+    availability: "Mon-Tue Evening 6:00-8:00 PM"
+  },
+  {
+    name: "Dr. Vidhi Rai",
+    email: "vidhi.rai@gmail.com",
+    password: bcrypt.hashSync("vidhi123", 10),
+    specialization: "Pets Animal Specialist",
+    contactNumber: "9833447013",
+    profileImage: "/images/female-doctor.png", // Replace with actual image URL or path
+    notes: "USA Return and well expert in pets staff care for a animals.",
+    isDoctor: true,
+    availability: "Sat-Sun Afternoon 1:00-3:00 PM"
+  }
+];
+
+export default doctors;
